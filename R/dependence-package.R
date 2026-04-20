232031fc64ebe00500812ba1e65768dc9b64a5eb
Matteo Pelagatti <matteo.pelagatti@unimib.it>
2026-04-20 13:11:29 +0200

    Initial commit

---
+++ R/dependence-package.R
@@ -0,0 +1,10 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @useDynLib dependence, .registration = TRUE
+## usethis namespace: end
+NULL
+
+#' @useDynLib indeptest, .registration = TRUE
+#' @importFrom Rcpp sourceCpp
